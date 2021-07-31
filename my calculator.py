0b4e5065aa89947a87f8b385ed7dda838c555d76
abdullah_sba <88181493+abdullah-sba@users.noreply.github.com>
2021-07-31 11:52:13 +0600

    Add files via upload

---
+++ my calculator.py
@@ -0,0 +1,86 @@
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+bwhite="\033[1;37m"       # White
+on_igreen="\033[0;102m"  #green
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+white="\033[0;37m"        # White
+
+ 
+   
+print(yellow+"--------------------")
+print(green+"code by Md Abdullah:")
+print(yellow+"--------------------")
+
+print("\n")
+print("------------------------------------------------------------")
+print(green +"""                
+                              _      _       _             
+                             | |    | |     | |            
+  _ __ ___  _   _    ___ __ _| | ___| |_   _| |_ ___  _ __ 
+ | '_ ` _ \| | | |  / __/ _` | |/ __| | | | | __/ _ \| '__|
+ | | | | | | |_| | | (_| (_| | | (__| | |_| | || (_) | |   
+ |_| |_| |_|\__, |  \___\__,_|_|\___|_|\__,_|\__\___/|_|   
+             __/ |                                 v1.0        
+            |___/                                          
+""")
+print(yellow+"------------------------------------------------------------")
+print("\n")
+print(blue+"✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭")
+print("\n")
+
+print(yellow+"Author      :",cyan+"MD Abdullah")
+print(yellow+"Facebool    :",cyan+"Termux hacking & basic bd ")
+print("            :",cyan+"Super bipap team")
+print("            :",cyan+"Bangladesh nobin spammer  ")
+print(yellow+"Youtub      :",cyan+"Termux hacking & basic bd  ")
+print("            : Super bipap")
+print("\n")
+print(blue+"☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆")
+
+
+print("\n")
+number1 =int(input(purple+"Enter your 1st number :")) 
+
+number2 =int(input(purple+"Enter your 2nd number :"))
+
+print(cyan+"\n")
+print("\t\t⟣⃟⸻⚀▞༻💙༺▞⚀⸻⃟⟢")
+print(yellow+"""
+                                               _ _   
+                                              | | |  
+  _   _  ___  _   _ _ __   _ __ ___  ___ _   _| | |_ 
+ | | | |/ _ \| | | | '__| | '__/ _ \/ __| | | | | __|
+ | |_| | (_) | |_| | |    | | |  __/\__ \ |_| | | |_ 
+  \__, |\___/ \__,_|_|    |_|  \___||___/\__,_|_|\__|
+   __/ |                                             
+  |___/                                              
+""")
+
+print(yellow+"____________________________________________________________")
+
+
+print("\n")
+result= number1 + number2
+print(blue+"pluse(+) =",result)
+
+result= number1 - number2
+print(blue+"minus(-) =",result)
+
+result= number1 * number2
+print(blue+"into(*)  =",result)
+
+result= number1 / number2
+print(blue+"devide(/)=",result)
+
+#program finished
